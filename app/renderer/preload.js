b49e45282dffc4b42d8437092e5d0d34cda89d27
xudafeng <xudafeng@126.com>
2021-12-08 12:17:13 +0800

    feat: add process info

--- app/renderer/preload.js
+++ app/renderer/preload.js
@@ -7,6 +7,11 @@ contextBridge.exposeInMainWorld(
   'electron',
   {
     openDevTool: () => getCurrentWindow().webContents.openDevTools(),
+    process: {
+      arch: process.arch,
+      version: process.version,
+      versions: process.versions,
+    },
   }
 );
 
